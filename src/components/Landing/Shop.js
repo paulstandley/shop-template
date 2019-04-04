994de579679a14915dfd0558a7085f86ec17ae93
Paul Standley <paulstandley1972@gmail.com>
2019-04-04 21:07:52 +0100

    add alt tags

--- src/components/Landing/Shop.js
+++ src/components/Landing/Shop.js
@@ -12,6 +12,13 @@ const Shop = (props) => {
         <Button variant="contained" color="primary">
           Hello World
         </Button>
+        <div class="carousel">
+          <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1" alt="nature"/></a>
+          <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2" alt="nature"/></a>
+          <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3" alt="nature"/></a>
+          <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4" alt="nature"/></a>
+          <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5" alt="nature"/></a>
+        </div>
       </main>
     </React.Fragment>
   );
