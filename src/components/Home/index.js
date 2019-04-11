190ce1bb7b1d9a9fc13e4f3f9b862db760d6231b
Paul Standley <paulstandley1972@gmail.com>
2019-04-11 22:11:11 +0100

    Style home page center google map

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 
 export class MapContainer extends Component {
   state = {
-    selectedPlace: {
-      name: "Shop"
-    }
+    selectedPlace: "Shop"
   }
 
   onMarkerClick(props, marker, e) {
@@ -18,35 +16,51 @@ export class MapContainer extends Component {
       width: '100%',
       height: '100%'
     }
+
     return (
-     <Main>
-     
-        <Map
-        google={this.props.google}
-        zoom={12}
-        style={style}
-        initialCenter={{
-          lat: 53.548532,
-          lng: -2.113063
-        }}>
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+      <React.Fragment>
         
-     </Main>
+        <Header>
+          <h1>We Are Hear</h1>
+        </Header>
+        <Main>
+          <Map
+            google={this.props.google}
+            zoom={12}
+            style={style}
+            value="We Are Hear"
+            initialCenter={{
+              lat: 53.548532,
+              lng: -2.113063
+            }}>
+            <Marker onClick={this.onMarkerClick}
+              name={'Current location'} />
+ 
+            <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                <h2>{this.state.selectedPlace}</h2>
+              </div>
+            </InfoWindow>
+          </Map>
+        
+        </Main>
+      </React.Fragment>
     );
   }
 }
 
+const Header = styled.header`
+  h1 {
+    text-align: center;
+    color: red;
+  }
+`;
+
 const Main = styled.main`
-  padding: auto;
-  margin: auto;
+  padding: 3rem 0 1rem 0; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default GoogleApiWrapper({
